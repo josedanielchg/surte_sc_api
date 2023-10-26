9f271f323fa2ed14ea824f26f54d8f3784a5cb04
Pedro Soto <pedrosotto717@gmail.com>
2023-10-26 03:05:57 -0400

    Added endpoint to get a station

--- routers/gas_stations.py
+++ routers/gas_stations.py
@@ -25,3 +25,27 @@ def get_gas_stations(db: Session = Depends(get_db), user_id: str = Depends(oauth
         )
 
     return gas_stations
+
+
+@router.get('/gas_stations/{station_id}')
+def get_gas_station_by_id(station_id: int, db: Session = Depends(get_db), user_id: str = Depends(oauth2.get_current_user)):
+    gas_station = (
+        db.query(models.GasStation)
+        .options(joinedload(models.GasStation.neighborhood))  # Carga relacionada con Neighborhood
+        .options(joinedload(models.GasStation.gas_supplies))  # Carga relacionada con GasSuply
+        .filter(models.GasStation.id == station_id)
+        .first()
+    )
+
+    if not gas_station:
+        raise HTTPException(status_code=404, detail="Gas station not found")
+
+    # Obtener el último UserReport relacionado con la estación de gasolina
+    gas_station.latest_report = (
+        db.query(models.UserReports)
+        .filter(models.UserReports.gas_stations_id == gas_station.id)
+        .order_by(models.UserReports.created_at.desc())
+        .first()
+    )
+
+    return gas_station
