c212179b75fccf0854c9646878c0c3d9be1b3776
Pedro Soto <pedrosotto717@gmail.com>
2023-10-26 01:45:18 -0400

    Creado sembrador de datos de relleno para las gas_stations

--- seeds.py
+++ seeds.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from seeds_data.gas_stations import gas_stations
 import models
 
 def seed_db(db: Session):
@@ -17,8 +18,32 @@ def seed_db(db: Session):
         db.commit()
     
     if db.query(models.GasStation).count() == 0:
-        # Seeding Users
-        user1 = models.User(email="john.doe@example.com", password="securepassword")
-        
-        db.add(user1)
+        # Recorrer el array gas_stations y crear los registros
+        for station in gas_stations:
+            # Verificar si el barrio ya existe
+            neighborhood = db.query(models.Neighborhood).filter(
+                models.Neighborhood.name == station["neighborhood_name"],
+                models.Neighborhood.city == station["city"],
+                models.Neighborhood.state == station["state"]
+            ).first()
+
+            # Si el barrio no existe, lo creamos
+            if not neighborhood:
+                neighborhood = models.Neighborhood(
+                    name=station["neighborhood_name"],
+                    city=station["city"],
+                    state=station["state"]
+                )
+                db.add(neighborhood)
+                db.commit()
+
+            # Crear la estación de gas y asociarla al barrio
+            gas_station = models.GasStation(
+                name=station["name"],
+                image_path=station["image"],
+                diesel=station["diesel"],
+                neighborhood_id=neighborhood.id
+            )
+            db.add(gas_station)
         db.commit()
+
--- seeds_data/gas_stations.py
+++ seeds_data/gas_stations.py
@@ -1,38 +1,38 @@
 gas_stations = [
   {
       "name": "Estacion de Servicio Libertador",
-      "latitude": 7.780897,
-      "longitude": -72.2699527,
+      "latitude": 7.782742002910019,
+      "longitude": -72.23344792291941,
       "image": "https://lh5.googleusercontent.com/p/AF1QipOE3LMXazzK1-MNiI1ey3aFSTP53RhfH0oGZRat=w408-h305-k-no",
-      "diesel": True,  # True if it has diesel, False otherwise
+      "diesel": True,
       "state": "Táchira",
       "city": "San Cristóbal",
       "neighborhood_name": ""
   },
   {
       "name": "Estacion de Servicio Cristo Rey",
-      "latitude": 7.7695327,
-      "longitude": -72.2525234,
+      "latitude": 7.770496053717281,
+      "longitude":  -72.21662510881218,
       "image": "https://lh5.googleusercontent.com/p/AF1QipN3eGNXKWHy1BGSuF99CmO5FCN2Fxq8ZStdErHz=w408-h306-k-no",
-      "diesel": True,  # True if it has diesel, False otherwise
+      "diesel": True,
       "state": "Táchira",
       "city": "San Cristóbal",
       "neighborhood_name": ""
   },
   {
       "name": "Estacion de Servicio Ana even",
-      "latitude": 7.7695327,
-      "longitude": -72.2525234,
+      "latitude": 7.776704114299613,
+      "longitude": -72.22683896023442,
       "image": "https://lh5.googleusercontent.com/p/AF1QipP-0wYJ4LQNvKtAYD_fz6j5mSet6M6fds-6-ANW=w408-h306-k-no",
-      "diesel": True,  # True if it has diesel, False otherwise
+      "diesel": True,
       "state": "Táchira",
       "city": "San Cristóbal",
       "neighborhood_name": ""
   },
   {
       "name": "Estación de Servicio 2 x 3",
-      "latitude": 7.7685127,
-      "longitude": -72.2513767,
+      "latitude": 7.769730670027863, 
+      "longitude": -72.21585263265419,
       "image": "https://lh5.googleusercontent.com/p/AF1QipP-0wYJ4LQNvKtAYD_fz6j5mSet6M6fds-6-ANW=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -41,8 +41,8 @@ gas_stations = [
   },
   {
       "name": "Estación De Servicio Rodelca",
-      "Latitud": 7.7816026, 
-      "Longitud": -72.248893,
+      "latitude": 7.783337282994116,  
+      "longitude": -72.21302022007491,
       "image": "https://lh5.googleusercontent.com/p/AF1QipO-Gutdes7S-cuDQB3BLHoHc8eIENunxT1b9bPV=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -51,8 +51,8 @@ gas_stations = [
   },
   {
       "name": "E/S PDV La Esperanza.",
-      "Latitud": 7.7559662, 
-      "Longitud": -72.2718409,
+      "latitude": 7.7571441604709985, 
+      "longitude":  -72.23370541497208,
       "image": "https://lh5.googleusercontent.com/p/AF1QipPZxmqb7c4Iujiy1aVUB75PU451fXiT3S06ElEG=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -61,8 +61,8 @@ gas_stations = [
   },
   {
       "name": "Estación de Servicio San Miguel",
-      "Latitud": 7.7559662,
-      "Longitud": -72.2718409,
+      "latitude": 7.771809450018797, 
+      "longitude": -72.23596778485233,
       "image": "https://lh5.googleusercontent.com/p/AF1QipNCVz415URRjKKmOmj1ByJ8bjnYzUUwSpGQhPKE=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -71,8 +71,8 @@ gas_stations = [
   },
   {
       "name": "Estación de Servicio El Terminal",
-      "Latitud": 7.7489234, 
-      "Longitud": -72.2758642,
+      "latitude": 7.750172589737807, 
+      "longitude": -72.23781501382854,
       "image": "https://lh5.googleusercontent.com/p/AF1QipMfDVULy-IupMS4D0zA126YAA46e6vAYhY7ROQq=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -81,8 +81,8 @@ gas_stations = [
   },
   {
       "name": "Estación De Servicio La Redoma",
-      "Latitud": 7.7804967,
-      "Longitud": -72.2767835,
+      "latitude": 7.781984906595631, 
+      "longitude": -72.23891278860906,
       "image": "https://lh5.googleusercontent.com/p/AF1QipMZ2mGYTNS55YKUaiHULX9HCku8L8Ewg7k-zTTg=w408-h544-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -91,8 +91,8 @@ gas_stations = [
   },
   {
       "name": "ESTACION DE SERVICIO SERVICENTRO",
-      "Latitud": 7.7489234, 
-      "Longitud": -72.2758642,
+      "latitude": 7.7622551103352855, 
+      "longitude": -72.23101636566076,
       "image": "https://lh5.googleusercontent.com/p/AF1QipMZ2mGYTNS55YKUaiHULX9HCku8L8Ewg7k-zTTg=w408-h544-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -101,8 +101,8 @@ gas_stations = [
   },
   {
       "name": "Estacion de Servicio Express La Concordia",
-      "Latitud": 7.7489234, 
-      "Longitud": -72.2758642,
+      "latitude": 7.75286000977789,
+      "longitude":  -72.2356117251791,
       "image": "https://lh5.googleusercontent.com/p/AF1QipN8K3A3cPrQ-XcoKUulIuWreHpkrNTz8o3iMqPw=w408-h725-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -111,8 +111,8 @@ gas_stations = [
   },
   {
       "name": "Estacion De Servicio Internacional",
-      "Latitud": 7.7521411,
-      "Longitud": -72.2623165,
+      "latitude": 7.753710470145029, 
+      "longitude": -72.23509674107373,
       "image": "https://lh5.googleusercontent.com/p/AF1QipMEXg2KhXssekqnrz-6uKR9S_oCqukezFQWLc9N=w426-h240-k-no",
       "diesel": True,  
       "state": "Táchira",
@@ -121,29 +121,80 @@ gas_stations = [
   },
   {
       "name": "Estacion De Servicio La Guacara",
-      "Latitud": 7.780897,
-      "Longitud": -72.2720126,
+      "latitude": 7.764511167215259, 
+      "longitude": -72.22788696359922,
       "image": "https://lh5.googleusercontent.com/p/AF1QipNQRGeh92lNS7_vFx_90vd07juYL4JwtsNgWb34=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
       "city": "San Cristóbal",
       "neighborhood_name": ""
   },
+  
   {
-      "name": "Estacion De Servicio La Guacara",
-      "Latitud": 7.780897,
-      "Longitud": -72.2720126,
-      "image": "https://lh5.googleusercontent.com/p/AF1QipNQRGeh92lNS7_vFx_90vd07juYL4JwtsNgWb34=w408-h306-k-no",
+      "name": "Estación De Servicio La Famosa de la Marginal",
+      "latitude": 7.763830816533529, 
+      "longitude": -72.24402313223268,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipPPzZof8PTMLBkb9SZTMIGtkdTLwmn4G8vr09Q=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
       "city": "San Cristóbal",
       "neighborhood_name": ""
   },
   {
-      "name": "",
-      "Latitud": 7.780897,
-      "Longitud": -72.2720126,
-      "image": "https://lh5.googleusercontent.com/p/AF1QipNQRGeh92lNS7_vFx_90vd07juYL4JwtsNgWb34=w408-h306-k-no",
+      "name": "Estación De Servicio UVCA",
+      "latitude": 7.75770761084375, 
+      "longitude": -72.22350959870397,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipOYlFKLnQaGYwcWGT_o3PLM1Yg0vUXYCaSVBfBd=w408-h291-k-no",
+      "diesel": True,  
+      "state": "Táchira",
+      "city": "San Cristóbal",
+      "neighborhood_name": ""
+  },
+  {
+      "name": "Estacion De Servicio Crislago",
+      "latitude": 7.75243477894982, 
+      "longitude": -72.23037605344162,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipOYlFKLnQaGYwcWGT_o3PLM1Yg0vUXYCaSVBfBd=w408-h291-k-no",
+      "diesel": True,  
+      "state": "Táchira",
+      "city": "San Cristóbal",
+      "neighborhood_name": ""
+  },
+  {
+      "name": "Estacion De Servicio",
+      "latitude": 7.767827861012444, 
+      "longitude": -72.24264984235211,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipOYlFKLnQaGYwcWGT_o3PLM1Yg0vUXYCaSVBfBd=w408-h291-k-no",
+      "diesel": True,  
+      "state": "Táchira",
+      "city": "San Cristóbal",
+      "neighborhood_name": ""
+  },
+  {
+      "name": "Estacion De Servicio La Rotaria",
+      "latitude": 7.745716074859502, 
+      "longitude": -72.22685699699413,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipP_xmnQw0dK1XaqfPw9fyRX2Bbd5hFh15DXko7g=w408-h306-k-no",
+      "diesel": True,  
+      "state": "Táchira",
+      "city": "San Cristóbal",
+      "neighborhood_name": ""
+  },
+  {
+      "name": "E/S Servicentro Los Llanos",
+      "latitude": 7.754220745540295,
+      "longitude": -72.2350967423983,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipPw0lsZ3qk7EINnB8MyYFHv6J-b7hg0Y4maeyId=w426-h240-k-no",
+      "diesel": True,  
+      "state": "Táchira",
+      "city": "San Cristóbal",
+      "neighborhood_name": ""
+  },
+  {
+      "name": "Estacion De Servicio Neymar",
+      "latitude": 7.777470888522729,
+      "longitude": -72.23619839580479,
+      "image": "https://lh5.googleusercontent.com/p/AF1QipNV9bwuXoav3dHUMPfVvNFvdplFRb-hOaONNOY7=w408-h306-k-no",
       "diesel": True,  
       "state": "Táchira",
       "city": "San Cristóbal",
