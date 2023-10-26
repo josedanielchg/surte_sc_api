3b51bca81a5c6e19de4e801eea1007647cc8efa6
Pedro Soto <pedrosotto717@gmail.com>
2023-10-26 00:54:45 -0400

    Fixed issue with user store

--- routers/user.py
+++ routers/user.py
@@ -14,16 +14,20 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
     # hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
+
+    userExist = db.query(models.User).filter(models.User.email == user.email).first()
+
+    if userExist:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"User is already exist")
 
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-
     return new_user
 
 
@@ -33,5 +37,5 @@ def get_user(id: int, db: Session = Depends(get_db), ):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"User with id: {id} does not exist")
-
+    
     return user
